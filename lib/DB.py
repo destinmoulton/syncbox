10a67d7d0fa593a64a909eafabf9fe4c29780d29
Destin Moulton <destin.moulton@gmail.com>
2022-01-20 11:41:19 -0700

    Added methods for sync folders

--- lib/DB.py
+++ lib/DB.py
@@ -1,19 +1,19 @@
 import os
-from appdirs import user_config_dir
 from tinydb import TinyDB, Query
+from lib.dirs import get_db_dir
 class DB():
     filename = 'config.json'
-    foldersTable = 'folders'
+    foldersTable = 'syncfolders'
 
     def __init__(self):
-        self.path = os.path.join(user_config_dir(),self.filename)
+        self.path = os.path.join(get_db_dir(),self.filename)
         self.db = TinyDB(self.path)
         return None
 
-    def addFolder(self,local,remote):
+    def addSyncFolder(self,local,remote):
         table = self.db.table(self.foldersTable)
         table.insert({'local':local, 'remote':remote})
 
-    def getAllFolders(self):
+    def getAllSyncFolders(self):
         table = self.db.table(self.foldersTable)
         return table.all()
