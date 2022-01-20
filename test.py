820d440452ccd9d2b95d99ba466ab1623f289d91
Destin Moulton <destin.moulton@gmail.com>
2022-01-19 21:50:49 -0700

    Main app loop

---
+++ test.py
@@ -0,0 +1,15 @@
+
+import sys
+from PySide2.QtGui import QIcon
+from PySide2.QtWidgets import QApplication, QSystemTrayIcon, QMenu, QAction, QPushButton, QMainWindow, QVBoxLayout
+from PySide2.QtCore import Qt
+from PySide2.QtGui import QPixmap, QIcon
+
+from gui.MainWindowView import MainWindowView
+from gui.systray.systray import systray
+
+app = QApplication(sys.argv)
+app.setQuitOnLastWindowClosed(False)
+
+window = MainWindowView()
+systray(app, window)
