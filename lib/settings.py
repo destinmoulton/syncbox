2e23d8d713d436799e59503ad859e8c06a75edfb
Destin Moulton <destin.moulton@gmail.com>
2022-01-20 20:53:33 -0700

    Removed settings parser

--- lib/settings.py
+++
@@ -1,7 +0,0 @@
-import configparser
-
-settingsfile = "settings.ini"
-def get_setting(section,key):
-    config = configparser.ConfigParser()
-    config.read(settingsfile)
-    return config.get(section,key)
